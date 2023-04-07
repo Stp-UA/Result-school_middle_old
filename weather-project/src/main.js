3cee95c93a7b9c33d703f8d388f9b4ce54f7c006
Stp_UA <stp@uatop.com>
2023-04-07 18:01:09 +0300

    Попытка восстановить проект после переноса папок

---
+++ weather-project/src/main.js
@@ -0,0 +1,72 @@
+import './clear.css'
+import './main.css'
+import './slider.css'
+import './decoration.css'
+
+let play = false // воспроизводится ли сейчас музыка?
+let prevSeason = '' // предыдущее состояние нажатой кнопки (было выбрано время года)
+
+const audio = new Audio()
+audio.loop = true // непрерывное воспроизведение
+
+const btns = document.querySelectorAll('.button') // кнопки в одном массиве
+const bg = document.querySelector('#bg') // объект для изменения картинки бэкграунда
+const vol = document.querySelector('.slider') // ползунок изменения громкости (inpur)
+const blur = document.querySelector('#blur') // слой поверх бэкграунда для блура картинки
+
+console.log(btns)
+
+function noiseChange(event) {
+  let div // указатель на кликнутый <div> для смены стилей
+  let img // указатель на кликнутый <img> для смены иконки
+  let season // кликнутое время года
+  const targ = event.target // элемент на котором кликнули
+  console.log(targ)
+  if (targ.id) { // если у элемента есть id - значит это <div>
+    div = targ
+    img = targ.firstElementChild
+  } else {
+    div = targ.parentElement
+    img = targ
+  }
+  season = `${div.id}` // eslint-disable-line
+
+  // обработка нажатий
+  if (play) { // идет воспроизведение
+    if (prevSeason === season) { // нажали ту-же самую кнопку
+      blur.classList.add('blur') // добавляем блюр фона
+      audio.pause()
+      play = false
+      img.src = `assets/icons/${season}.svg` // возвращаем иконку времени года
+      prevSeason = '' // никакой сезон не выбран
+      for (const btn of btns) { // делаем возможным нажатие любых кнопок
+        btn.classList.add('rotate')
+        btn.classList.remove('opa')
+      }
+    }
+  } else {
+    bg.className = season // прописываем класс времени года в бэкграунд
+    blur.classList.remove('blur') // добавляем блюр фона
+    audio.src = `assets/sounds/${season}.mp3`
+    audio.play() // включаем воспроизведение звуков
+    img.src = 'assets/icons/pause.svg' // вешаем иконку паузы
+    play = true
+    prevSeason = season // запоминаем какое время года воспроизводится
+    for (const btn of btns) { // запрещаем нажатие всех кнопок
+      btn.classList.remove('rotate')
+      btn.classList.add('opa')
+    }
+    div.classList.add('rotate') // разрешаем нажатие для нужного сезона
+    div.classList.remove('opa')
+  }
+}
+
+// функция обрабатывающая регулятор громкости
+vol.onclick = function (event) {
+  audio.volume = +vol.value / 100
+}
+
+// вешаем на кнопки обрабочик нажатия
+for (const btn of btns) {
+  btn.addEventListener('click', noiseChange)
+}
---
+++ weather-project/webpack.config.common.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './main.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/static/index.html')
+    }),
+    new MiniCssExtractPlugin()
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('postcss-preset-env')]
+              }
+            }
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(mp3)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  }
+}
